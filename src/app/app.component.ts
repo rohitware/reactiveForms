e25ff45cfc5ae5f2fb9c97489289896642be6536
Rohit ware <rohitware99@gmail.com>
2022-09-28 01:39:13 +0530

    FormBuilder Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,38 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-
+// import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  registrationForm = new FormGroup({
-    userName: new FormControl('Rohit'),
-    password: new FormControl(),
-    confirmPassword: new FormControl(),
+  registrationForm: any;
 
-    address: new FormGroup({
-      city: new FormControl(''),
-      state: new FormControl(''),
-      postalcode: new FormControl('')
-    })
-  });
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      userName: ['Rohit'],
+      password: [],
+      confirmPassword: [],
+
+      address: this.fb.group({
+        city: [],
+        state: [],
+        postalcode: []
+      })
+    });
+  }
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('Rohit'),
+  //   password: new FormControl(),
+  //   confirmPassword: new FormControl(),
+
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalcode: new FormControl('')
+  //   })
+  // });
 
   loadApiData() {
     this.registrationForm.setValue({
@@ -25,12 +40,12 @@ export class AppComponent {
       password: 'test',
       confirmPassword: 'test',
 
-      address:{
+      address: {
         city: 'chincholi',
         state: 'maharashtra',
         postalcode: '411037'
       }
-      
+
     });
   }
 }
